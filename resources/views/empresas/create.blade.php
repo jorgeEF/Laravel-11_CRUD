4aa6c62ac439c15f4ab86074193e8b3995856ef5
JFEMENIA <jorgefemenia@gmail.com>
2024-06-03 15:09:55 -0300

    mostrar errores en create

--- resources/views/empresas/create.blade.php
+++ resources/views/empresas/create.blade.php
@@ -21,22 +21,37 @@
             <div class="form-group">
                 <label for="razon_social">Razon Social:</label>
                 <input type="text" class="form-control" id="razon_social" name="razon_social">
+                @error('razon_social')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="nombre_fantasia">Nombre de Fantasía:</label>
                 <input type="text" class="form-control" id="nombre_fantasia" name="nombre_fantasia">
+                @error('nombre_fantasia')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="cuit">CUIT:</label>
                 <input type="text" class="form-control" id="cuit" name="cuit">
+                @error('cuit')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="text" class="form-control" id="email" name="email">
+                @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="estado">Estado:</label>
                 <input type="text" class="form-control" id="estado" name="estado">
+                @error('estado')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group mt-2">
                 <input type="submit" class="btn btn-primary" value="Crear empresa">
